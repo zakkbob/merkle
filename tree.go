2cbbee5e4456e99aad74ede65fcb6ffc6fd2ec09
zakkbob <114868973+zakkbob@users.noreply.github.com>
2025-10-04 01:00:19 +0100

    Add Tree
    Add ability to create a merkle tree from a string slice. Additionally, add a String() method on Tree

--- tree.go
+++ tree.go
@@ -1,9 +1,96 @@
 package merkle
 
+import (
+	"encoding/hex"
+	"strings"
+)
+
+type Tree struct {
+	// map containing leafs and their indexes
+	// root node
+	// json encode/decode
+	root Node
+}
+
+func NewTree(leaves []string, hashFn func([]byte) []byte) Tree {
+	nodes := make([]*Node, len(leaves))
+	for i, h := range leaves {
+		nodes[i] = &Node{
+			Left:  nil,
+			Right: nil,
+			Hash:  h,
+		}
+	}
+
+	for len(nodes) != 1 {
+		l := len(nodes)
+		for i := range l / 2 {
+			left, right := nodes[2*i], nodes[2*i+1]
+			nodes[i] = NewNode(left, right, hashFn)
+		}
+		if l%2 == 1 {
+			nodes[l/2] = nodes[l-1]
+		}
+		nodes = nodes[:(l/2 + l%2)]
+	}
+
+	return Tree{
+		root: *nodes[0],
+	}
+}
+
+func (t *Tree) String() string {
+	b := &strings.Builder{}
+	t.root.BuildString(0, []bool{}, false, b)
+	return b.String()
+}
+
+func (t *Tree) Prove(hash string) []string {
+	return t.root.Prove(hash)
+}
+
 type Node struct {
 	Left  *Node
 	Right *Node
 	Hash  string
+}
+
+func NewNode(left *Node, right *Node, hashFn func([]byte) []byte) *Node {
+	return &Node{
+		Left:  left,
+		Right: right,
+		Hash:  string(hashFn(append([]byte(left.Hash), []byte(right.Hash)...))),
+	}
+}
+
+func (n *Node) BuildString(depth int, lines []bool, leftNode bool, builder *strings.Builder) {
+	for i := range depth {
+		if lines[i] {
+			builder.WriteString("│  ")
+
+		} else {
+			builder.WriteString("   ")
+
+		}
+	}
+
+	if leftNode {
+		builder.WriteString("├─ ")
+	} else {
+		builder.WriteString("└─ ")
+	}
+
+	builder.WriteString(hex.EncodeToString([]byte(n.Hash)))
+	builder.WriteByte('\n')
+
+	if n.IsLeaf() {
+		return
+	}
+
+	lines = append(lines, leftNode)
+
+	n.Left.BuildString(depth+1, lines, true, builder)
+	n.Right.BuildString(depth+1, lines, false, builder)
 }
 
 func (n *Node) IsLeaf() bool {
--- tree_test.go
+++ tree_test.go
@@ -1,46 +1,54 @@
 package merkle_test
 
 import (
+	"crypto/sha256"
 	"slices"
 	"testing"
 
 	"github.com/zakkbob/merkle"
 )
 
-func TestProve(t *testing.T) {
-	// a
-	// ├─ b
-	// │  ├─ c
-	// │  └─ d
-	// └─ e
+func TestNewTree(t *testing.T) {
+	tree := merkle.NewTree([]string{"a", "b", "c", "d", "e", "f"}, func(data []byte) []byte {
+		b := sha256.Sum256(data)
+		return b[:]
+	})
 
-	e := merkle.Node{Hash: "e"}
-	d := merkle.Node{Hash: "d"}
-	c := merkle.Node{Hash: "c"}
-	b := merkle.Node{Hash: "b", Left: &c, Right: &d}
-	a := merkle.Node{Hash: "a", Left: &b, Right: &e}
+	t.Log("\n" + tree.String())
+}
+
+func TestProve(t *testing.T) {
+	// abc
+	// ├─ ab
+	// │  ├─ a
+	// │  └─ b
+	// └─ c
+
+	tree := merkle.NewTree([]string{"a", "b", "c"}, func(b []byte) []byte {
+		return b
+	})
 
 	tests := []struct {
 		Target   string
 		Expected []string
 	}{
 		{
-			Target:   "e",
-			Expected: []string{"e", "b"},
+			Target:   "a",
+			Expected: []string{"a", "b", "c"},
 		},
 		{
-			Target:   "d",
-			Expected: []string{"d", "c", "e"},
+			Target:   "b",
+			Expected: []string{"b", "a", "c"},
 		},
 		{
 			Target:   "c",
-			Expected: []string{"c", "d", "e"},
+			Expected: []string{"c", "ab"},
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.Target, func(t *testing.T) {
-			p := a.Prove(tt.Target)
+			p := tree.Prove(tt.Target)
 			if !slices.Equal(p, tt.Expected) {
 				t.Fatalf("got %v; expected %v", p, tt.Expected)
 			}
